ba140744ab58de76644d315db74671ee261bb549
Tian Zhao <kastrcn@outlook.com>
2020-08-31 10:58:01 +0800

    init

---
+++ ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace GoogleTranslate
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            webView.LoadRequest(new NSUrlRequest(new NSUrl("https://translate.google.cn/")));
+
+
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Google Translate
+使用Xamarin 发布版本30M  
+
+使用原生编写200K
+
+
+![image-20200831105652570](./docs/img/img.png)
